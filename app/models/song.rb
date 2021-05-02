94681da047b58ca0f0680d93ef15d1184457a83f
Michael Nnaji <mnnaji3@uwo.ca>
2021-05-02 16:55:21 +0000

    Automatically backed up by Learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,16 +1,13 @@
 class Song < ApplicationRecord
-  validates :title, presence: true, uniqueness: true
-  validates :released, presence: true
-  validates :artist_name, presence: true
-  scope: %i[release_year artist_name],
-    message: 'I think you are lying. Only prince could be that prolific...'
+  validates :title, presence: true
+  validates :title, uniqueness: {
+    scope: %i[release_year artist_name],
+    message: 'cannot be repeated by the same artist in the same year'
   }
-
   validates :released, inclusion: { in: [true, false] }
-
   validates :artist_name, presence: true
 
-    with_options if: :released? do |song|
+  with_options if: :released? do |song|
     song.validates :release_year, presence: true
     song.validates :release_year, numericality: {
       less_than_or_equal_to: Date.today.year
@@ -21,3 +18,6 @@ class Song < ApplicationRecord
     released
   end
 end
+
+
+
