d00b4fbb55f31f200d6bc0b3b8152219188775b6
Michael Nnaji <mnnaji3@uwo.ca>
2021-05-02 16:59:28 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,2 +1,18 @@
 class SongsController < ApplicationController
+
+  def new 
+    @song = Song.new
+  end
+
+  def create 
+    @song = Song.new(song_params(:title,:released,:release_year,:artist_name,:genre))
+    if @song.valid?
+     @song.save
+     redirect_to song_path(@song)
+    else 
+      render :new 
+    end
+  end
+   
+    
 end
