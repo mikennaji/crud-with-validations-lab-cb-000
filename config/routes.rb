33196d4eeff0816b6d16796011d4aa2d2980c67d
Michael Nnaji <mnnaji3@uwo.ca>
2021-05-02 20:08:24 +0000

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  resources :songs
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+delete '/song/:id', to: 'song#destroy'
 end
